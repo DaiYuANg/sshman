452ccd8fd7117d9fb66dda2a713d6db028ef6f68
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2025-07-28 00:09:38 +0800

    change tauri macos window

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 mod app_state;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -5,11 +7,51 @@ mod app_state;
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+use cocoa::appkit::{NSWindow, NSWindowStyleMask};
+use tauri::{Runtime, Window};
 
+pub trait WindowExt {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, transparent: bool);
+}
+
+impl<R: Runtime> WindowExt for Window<R> {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, transparent: bool) {
+    use cocoa::appkit::NSWindowTitleVisibility;
+
+    unsafe {
+      let id = self.ns_window().unwrap() as cocoa::base::id;
+
+      let mut style_mask = id.styleMask();
+      style_mask.set(
+        NSWindowStyleMask::NSFullSizeContentViewWindowMask,
+        transparent,
+      );
+      id.setStyleMask_(style_mask);
+
+      id.setTitleVisibility_(if transparent {
+        NSWindowTitleVisibility::NSWindowTitleHidden
+      } else {
+        NSWindowTitleVisibility::NSWindowTitleVisible
+      });
+      id.setTitlebarAppearsTransparent_(if transparent {
+        cocoa::base::YES
+      } else {
+        cocoa::base::NO
+      });
+    }
+  }
+}
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-
     tauri::Builder::default()
+        .setup(|app| {
+            let win = app.get_window("main").unwrap();
+            win.set_transparent_titlebar(true);
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,29 +1,126 @@
 import {RefObject} from "react";
 import "./App.css";
 import {useXTerm} from "react-xtermjs";
+import {
+  Button, Card, CardBody,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Tab,
+  Tabs,
+  useDisclosure
+} from "@heroui/react";
+import {Panel, PanelGroup, PanelResizeHandle} from "react-resizable-panels";
 
 const MyTerminal = () => {
-    const {instance, ref} = useXTerm()
-    instance?.writeln('Hello from react-xtermjs!')
-    instance?.onData((data) => instance?.write(data))
+  const {instance, ref} = useXTerm()
+  instance?.writeln('Hello from react-xtermjs!')
+  instance?.onData((data) => instance?.write(data))
 
-    return <div ref={ref as RefObject<HTMLDivElement>} style={{width: '100%', height: '100%'}}/>
+  return <div ref={ref as RefObject<HTMLDivElement>} style={{width: '100%', height: '100%'}}/>
 }
+import { getCurrentWindow } from '@tauri-apps/api/window';
 
-function App() {
-    // const [greetMsg, setGreetMsg] = useState("");
-    // const [name, setName] = useState("");
+// when using `"withGlobalTauri": true`, you may use
+// const { getCurrentWindow } = window.__TAURI__.window;
 
-    // async function greet() {
-    //     Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-        // setGreetMsg(await invoke("greet", {name}));
-    // }
+const appWindow = getCurrentWindow();
 
-    return (
-        <main className="container">
-            <MyTerminal/>
-        </main>
-    );
-}
+document
+  .getElementById('titlebar-minimize')
+  ?.addEventListener('click', () => appWindow.minimize());
+document
+  .getElementById('titlebar-maximize')
+  ?.addEventListener('click', () => appWindow.toggleMaximize());
+document
+  .getElementById('titlebar-close')
+  ?.addEventListener('click', () => appWindow.close());
+const App = () => {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  return (
+    <main className="container h-screen w-full">
+      <PanelGroup direction="horizontal" className="h-full">
+        {/* 左侧栏 */}
+        <Panel defaultSize={25} minSize={15} maxSize={50}>
+          <div data-tauri-drag-region className="h-full mt-2  p-4  border-gray-300">
+            {/*<h2 className="text-lg font-bold mb-2">左侧栏</h2>*/}
+            <Button onPress={onOpen}>test button</Button>
+          </div>
+        </Panel>
+
+        {/* 拖拽手柄 */}
+        <PanelResizeHandle className="w-1 bg-gray-300 hover:bg-gray-400 cursor-col-resize"/>
+
+        {/* 右侧主区域 */}
+        <Panel>
+          <div className="h-full p-4 overflow-auto bg-white">
+            <Tabs aria-label="Options">
+              <Tab key="photos" title="Photos">
+                <h2 className="text-lg font-bold mb-2">终端</h2>
+                <MyTerminal/>
+              </Tab>
+              <Tab key="music" title="Music">
+                <Card>
+                  <CardBody>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur.
+                  </CardBody>
+                </Card>
+              </Tab>
+              <Tab key="videos" title="Videos">
+                <Card>
+                  <CardBody>
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.
+                  </CardBody>
+                </Card>
+              </Tab>
+            </Tabs>
+
+          </div>
+        </Panel>
+      </PanelGroup>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                  quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                  quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
+                  adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
+                  officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                  nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
+                  deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </main>
+  );
+};
 
 export default App;
