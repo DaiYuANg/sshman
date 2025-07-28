2f95070ebfbde47799e4fdce4e0c40e9bd906a73
ddddd <138393979+DaiYuANg@users.noreply.github.com>
2025-07-28 11:10:04 +0800

    fix macos window customization

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -7,48 +7,52 @@ mod app_state;
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-use cocoa::appkit::{NSWindow, NSWindowStyleMask};
+
 use tauri::{Runtime, Window};
 
 pub trait WindowExt {
-  #[cfg(target_os = "macos")]
-  fn set_transparent_titlebar(&self, transparent: bool);
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self, transparent: bool);
 }
 
 impl<R: Runtime> WindowExt for Window<R> {
-  #[cfg(target_os = "macos")]
-  fn set_transparent_titlebar(&self, transparent: bool) {
-    use cocoa::appkit::NSWindowTitleVisibility;
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self, transparent: bool) {
+        use cocoa::appkit::NSWindowTitleVisibility;
 
-    unsafe {
-      let id = self.ns_window().unwrap() as cocoa::base::id;
+        unsafe {
+            let id = self.ns_window().unwrap() as cocoa::base::id;
 
-      let mut style_mask = id.styleMask();
-      style_mask.set(
-        NSWindowStyleMask::NSFullSizeContentViewWindowMask,
-        transparent,
-      );
-      id.setStyleMask_(style_mask);
+            let mut style_mask = id.styleMask();
+            style_mask.set(
+                NSWindowStyleMask::NSFullSizeContentViewWindowMask,
+                transparent,
+            );
+            id.setStyleMask_(style_mask);
 
-      id.setTitleVisibility_(if transparent {
-        NSWindowTitleVisibility::NSWindowTitleHidden
-      } else {
-        NSWindowTitleVisibility::NSWindowTitleVisible
-      });
-      id.setTitlebarAppearsTransparent_(if transparent {
-        cocoa::base::YES
-      } else {
-        cocoa::base::NO
-      });
+            id.setTitleVisibility_(if transparent {
+                NSWindowTitleVisibility::NSWindowTitleHidden
+            } else {
+                NSWindowTitleVisibility::NSWindowTitleVisible
+            });
+            id.setTitlebarAppearsTransparent_(if transparent {
+                cocoa::base::YES
+            } else {
+                cocoa::base::NO
+            });
+        }
     }
-  }
 }
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let win = app.get_window("main").unwrap();
-            win.set_transparent_titlebar(true);
+            #[cfg(target_os = "macos")]
+            {
+                use cocoa::appkit::{NSWindow, NSWindowStyleMask};
+                let win = app.get_window("main").unwrap();
+                win.set_transparent_titlebar(true);
+            }
 
             Ok(())
         })
