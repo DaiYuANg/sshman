21657efe0767155f5997812a600cde187bb8dd1e
ddddd <138393979+DaiYuANg@users.noreply.github.com>
2025-07-28 17:35:54 +0800

    windows 更新 逻辑

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import {HeroUIProvider} from "@heroui/react";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+const appWindow = getCurrentWindow();
 
+document
+  .getElementById('titlebar-minimize')
+  ?.addEventListener('click', () => appWindow.minimize());
+document
+  .getElementById('titlebar-maximize')
+  ?.addEventListener('click', () => appWindow.toggleMaximize());
+document
+  .getElementById('titlebar-close')
+  ?.addEventListener('click', () => appWindow.close());
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HeroUIProvider>
